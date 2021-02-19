d5a404dbc9f65145f76f2f297fefe86fcd75c4ec
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2021-02-19 20:06:49 +0200

    Data Exploration

---
+++ Regression_model.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 19 18:50:45 2021
+
+@author: GHOST
+"""
+
+import pandas as pd
+import numpy as np
+
+
+
+# Data Exploration
+
+cars_data = pd.read_csv(r'D:\GitHub\data\US_Cars_Regression\USA_cars_datasets.csv') # read dataset
+
+dataset_shape = cars_data.shape
+
+print(cars_data.head(10)) # Top 10 details in cars dataset
+
+column_names = cars_data.columns # Name of the columns
+
+
+
